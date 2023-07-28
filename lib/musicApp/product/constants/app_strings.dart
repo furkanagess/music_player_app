b43e8908f9b0f37d772f351cc1abeb11a589b991
furkanagess <caglarrfurkann@gmail.com>
2023-07-28 23:43:28 +0300

    string fixes

--- lib/musicApp/product/constants/app_strings.dart
+++ lib/musicApp/product/constants/app_strings.dart
@@ -1,19 +1,45 @@
-class MusicAppStrings {
-  String closeAd = "Reklamları Kapat";
-  String monthly = "19.99₺ Aylık";
-  String next = "Devam et";
-  String reachProperties = "Tüm özelliklere eriş. Mükemmel bir Player \n deneyimine hazır ol!";
-  String reachProperties2 = "Tüm özelliklere eriş. Mükemmel bir Player \n deneyimine hazır ol!  Tüm özelliklere eriş. \n deneyimine hazır ol!";
-  String search = "Search";
-  String musicTitle = "Kalbin Yok Mu";
-  String musicSubTitle = "Marc Aryan";
-  String recent = "Recent";
-  String header = "Merhaba, iyi günler";
-  String playNext = "Sonrakini Çal";
-  String addMusic = "Parçalara Ekle";
-  String addList = "Oynatma Listesine Ekle";
-  String repeatAll = "Tümünü Tekrarla";
-  String sleepTimer = "Uyku Zamanlayıcı";
-  String deleteList = "Çalma Listesinden Sil";
-  String report = "İçeriği Raporla";
+final class AppStrings {
+  static const appName = 'Video Player App';
+  static const closeAd = 'Reklamları Kapat';
+  static const monthly = '19.99₺ Aylık';
+  static const next = 'Devam et';
+  static const reachProperties = '''
+Tüm özelliklere eriş. Mükemmel bir Player 
+   deneyimine hazır ol!''';
+  static const reachProperties2 = '''
+Tüm özelliklere eriş. Mükemmel bir Player 
+  \n deneyimine hazır ol!  Tüm özelliklere eriş. 
+  \n deneyimine hazır ol!''';
+  static const search = 'Search';
+  static const musicTitle = 'Kalbin Yok Mu';
+  static const musicSubTitle = 'Marc Aryan';
+  static const recent = 'Recent';
+  static const header = 'Merhaba, iyi günler';
+  static const playNext = 'Sonrakini Çal';
+  static const addMusic = 'Parçalara Ekle';
+  static const addList = 'Oynatma Listesine Ekle';
+  static const repeatAll = 'Tümünü Tekrarla';
+
+  static const deleteList = 'Çalma Listesinden Sil';
+  static const report = 'İçeriği Raporla';
+  static const ads = 'ADS';
+
+  // Create Playlist Bottom Sheet
+  static const createPlaylist = 'Oynatma Listesi \n        Oluştur';
+  static const playlistName = 'Oynatma Listesi Adı';
+  static const giveName = 'İsimlendir';
+
+  // Drawer Hints
+  static const library = 'Library';
+  static const equalizer = 'Equalizer';
+  static const sleepTimer = 'Sleep Timer';
+  static const theme = 'Theme';
+  static const adsOff = 'Ads Off';
+  static const ringtones = 'Ringtones';
+  static const settings = 'Settings';
+
+  // Chips
+  static const musics = 'Musics';
+  static const playlists = 'Playlists';
+  static const downloads = 'Downloads';
 }
