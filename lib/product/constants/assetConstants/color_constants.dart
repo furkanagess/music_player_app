fe19495873a8543bd6bbe74ab0d8650e51d2105e
furkanagess <caglarrfurkann@gmail.com>
2023-08-03 02:17:13 +0300

    color_constants fixes

--- lib/product/constants/assetConstants/color_constants.dart
+++ lib/product/constants/assetConstants/color_constants.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 @immutable
 final class AppColors {
-  Color get purple => const Color(0xff6e4bf5);
-  Color get pink => const Color(0xffea489f);
-  Color get orange => const Color(0xfff5735f);
-  Color get yellow => const Color(0xfffdd776);
-  Color get white => Colors.white;
-  Color get background => const Color(0xff0e0e14);
-  Color get transparentWhite => const Color.fromARGB(20, 255, 255, 255);
-  Color get lightWhite => const Color.fromARGB(50, 255, 255, 255);
-  Color get darkBlue => const Color.fromARGB(255, 34, 34, 49);
+  const AppColors._();
+  static const purple = Color(0xff6e4bf5);
+  static const pink = Color(0xffea489f);
+  static const orange = Color(0xfff5735f);
+  static const yellow = Color(0xfffdd776);
+  static const white = Colors.white;
+  static const transparent = Colors.transparent;
+  static const background = Color(0xff0e0e14);
+  static const transparentWhite = Color.fromARGB(20, 255, 255, 255);
+  static const lightWhite = Color.fromARGB(50, 255, 255, 255);
+  static const darkBlue = Color.fromARGB(255, 34, 34, 49);
 }
